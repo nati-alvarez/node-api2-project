70f1ae8d6cbc3e6b48c90ea4bcbb1400d9a324f4
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-07 01:21:51 -0400

    Created /api/posts and /api/posts/:id get endpoints

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("../data/db");
+
 router.get("/", (req, res)=>{
-    res.send("posts index route");
+    db.find().then(posts=>{
+        res.status(200).json(posts);
+    }).catch(err=>{
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+})
+
+router.get("/:id", (req, res)=>{
+    db.findById(req.params.id).then(post=>{
+        res.status(200).json(post[0]);
+    }).catch(err=>{
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+    })
 })
 
 module.exports = router;
