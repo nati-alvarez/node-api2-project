2e451c99bc1991993ac149c67fb116bff0c785b1
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-07 01:37:51 -0400

    Created /api/posts post endpoint

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -11,6 +11,19 @@ router.get("/", (req, res)=>{
     })
 })
 
+router.post("/", (req, res)=>{
+    const post = req.body;
+    if(!post.title || ! post.contents){
+        return res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+
+    db.insert(post).then(newPostId=>{
+        res.status(201).json({id: newPostId.id, ...req.body});
+    }).catch(err=>{
+        res.status(500).json({ error: "There was an error while saving the post to the database"});
+    });
+});
+
 router.get("/:id", (req, res)=>{
     db.findById(req.params.id).then(post=>{
         res.status(200).json(post[0]);
